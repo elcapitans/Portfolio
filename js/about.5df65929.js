3c02cdce2856bc7dfa4a97b68cb81cb52c0c6930
Ben <ben.d.chewning@gmail.com>
2024-10-19 02:11:31 -0400

    Updates

---
+++ js/about.5df65929.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkportfolio"]=self["webpackChunkportfolio"]||[]).push([[594],{603:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var o=u(6768);const r={class:"about"};function a(n,t){return(0,o.uX)(),(0,o.CE)("div",r,t[0]||(t[0]=[(0,o.Lk)("h1",null,"This is an about page",-1)]))}var e=u(1241);const s={},c=(0,e.A)(s,[["render",a]]);var i=c}}]);
+//# sourceMappingURL=about.5df65929.js.map
